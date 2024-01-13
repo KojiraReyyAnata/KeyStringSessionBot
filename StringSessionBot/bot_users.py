211515a641effd543dbaeed98b97328827e34475
Keysmith <113760925+KojiraReyyAnata@users.noreply.github.com>
2024-01-14 03:58:19 +0700

    Ngentod

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -1,17 +1,17 @@
 from pyrogram.types import Message
 from pyrogram import Client, filters
 
-from env import DATABASE_URL
+from env import MONGO_URI
 
 
-if DATABASE_URL != '':
-    from StringSessionBot.database import SESSION
-    from StringSessionBot.database.users_sql import Users, num_users
+if MONGO_URI != '':
+    from StringSessionBot.mongo import SESSION
+    from StringSessionBot.mongo.db import Users, num_users
 
 
 @Client.on_message(~filters.service, group=1)
-async def users_sql(_, msg: Message):
-    if DATABASE_URL == '':
+async def mongo_db(_, msg: Message):
+    if MONGO_URI == '':
         return
     if msg.from_user:
         q = SESSION.query(Users).get(int(msg.from_user.id))
