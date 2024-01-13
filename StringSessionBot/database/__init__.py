9bc22950e611c97ce4a043b74c6ae5800eeb15cb
Keysmith <113760925+KojiraReyyAnata@users.noreply.github.com>
2024-01-14 03:54:40 +0700

    Ngentod

--- StringSessionBot/database/__init__.py
+++ StringSessionBot/database/__init__.py
@@ -1,21 +1,21 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+from mongo.db import create_engine
+from mongo.ext.declarative import declarative_base
+from mongo.orm import sessionmaker, scoped_session
 
-from env import DATABASE_URL
+from env import MONGO_URI
 
 count_ = 0
 def start() -> scoped_session:
     if DATABASE_URL == "":
         if count_ < 1:
             count += 1
-            return print("Database url not provided..\nBut this time I won't stop 😉")
+            return print("MONGO uri not provided..\nBut this time I won't stop 😉")
         return
-    engine = create_engine(DATABASE_URL)
+    engine = create_engine(MONGO_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-if DATABASE_URL != "":
+if MONGO_URI != "":
     BASE = declarative_base()
     SESSION = start()
