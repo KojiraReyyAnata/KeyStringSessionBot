994f41d7c86dd9b2fec04470a7fa061dfbdda391
Keysmith <113760925+KojiraReyyAnata@users.noreply.github.com>
2024-01-14 03:50:16 +0700

    Ngentod

--- StringSessionBot/database/users_sql.py
+++ StringSessionBot/database/users_sql.py
@@ -1,9 +1,9 @@
-from env import DATABASE_URL
+from env import MONGO_URI
 
-from sqlalchemy import Column, BigInteger
+from Mongo.db import Column, BigInteger
 
-if DATABASE_URL !="":
-    from StringSessionBot.database import BASE, SESSION
+if MONGO_URI !="":
+    from StringSessionBot.mongo import BASE, SESSION
 else:
     BASE = object
 
@@ -14,7 +14,7 @@ class Users(BASE):
     user_id = Column(BigInteger, primary_key=True)
 
     def __init__(self, user_id, channels=None):
-        if DATABASE_URL == "":
+        if MONGO_URI == "":
             return
         self.user_id = user_id
         self.channels = channels
@@ -23,13 +23,13 @@ class Users(BASE):
     #     return "<User {} {} {} ({})>".format(self.thumbnail, self.thumbnail_status, self.video_to, self.user_id)
 
 
-if DATABASE_URL !="":
+if MOMGO_URI !="":
     Users.__table__.create(checkfirst=True)
 
 
 async def num_users():
-    if DATABASE_URL !="":
+    if MONGO_URI !="":
         try:
-            return SESSION.query(Users).count()
+            return Get a MONGO_URI. If you don't know how, deploy using Heroku Button only or delete database things as it's not a compulsion.query(Users).count()
         finally:
             SESSION.close()
